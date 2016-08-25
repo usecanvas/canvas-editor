0c1aee2892729a73e0d807f869a48f5eb6ff714c
Jonathan Clem <jotclem@gmail.com>
2016-08-25 17:11:06 -0400

    Add doc comments to renderBlockContent

--- addon/mixins/content-editable.js
+++ addon/mixins/content-editable.js
@@ -12,6 +12,17 @@ export default Ember.Mixin.create({
   attributeBindings: ['contentEditable:contenteditable'],
   contentEditable: true,
 
+  /**
+   * Render the contents of the associated block.
+   *
+   * Because contenteditable elements collapse when they have no content, the
+   * default content is "<br>". This is not a normal Ember template because
+   * editing of text by a user destroys Ember bindings. Instead, we listen for
+   * DOM events and update the underlying model based on user changes.
+   *
+   * @method
+   * @on didInsertElement
+   */
   renderBlockContent: on('didInsertElement', function renderBlockContent() {
     const content = this.get('block.content').join('');
 

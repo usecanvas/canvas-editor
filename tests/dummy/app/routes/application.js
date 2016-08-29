4f1f53a5cdba1c2145fa857623041118f860daa0
Matt Sacks <matt.s.sacks@gmail.com>
2016-08-29 15:58:13 -0700

    Add string of dummy text for styling line-height

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -20,7 +20,8 @@ export default Ember.Route.extend({
         Paragraph.create({ content: A(['Hello, World']) }),
         Paragraph.create({ content: A(['Hello, World']) }),
         Paragraph.create({ content: A(['Hello, World']) }),
-        Paragraph.create({ content: A(['Foo Bar Baz']) })
+        Paragraph.create({ content: A(['Foo Bar Baz']) }),
+        Paragraph.create({ content: A(["Kitsch +1 tousled cliche. Lumbersexual distillery keffiyeh flannel, green juice put a bird on it ugh williamsburg mixtape art party. Yuccie skateboard bicycle rights cardigan, kickstarter vinyl knausgaard mumblecore four loko wayfarers salvia before they sold out. Flannel gluten-free pabst umami. Hammock before they sold out deep v, selfies knausgaard normcore gluten-free chambray helvetica readymade trust fund cliche health goth. Literally twee poutine umami, forage next level crucifix gluten-free ennui. Banjo +1 quinoa blue bottle cray tilde, selvage church-key meditation lomo sriracha bitters truffaut tattooed."]) })
       ])
     });
   }

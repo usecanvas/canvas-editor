0ddd0b93fcf4d652e26bb2a8f4ceac284eed1948
Jonathan Clem <jotclem@gmail.com>
2016-08-29 12:22:54 -0400

    Focus block on vertical navigation

--- addon/lib/selection.js
+++ addon/lib/selection.js
@@ -128,6 +128,7 @@ const SelectionService = Ember.Object.extend({
     }
 
     range = new Rangy.WrappedRange(range);
+    blockElement.focus();
     this.get('selection').setSingleRange(range);
   }
 });

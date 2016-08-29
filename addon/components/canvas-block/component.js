cb3b85492269e46c80796234e608fcb7312a59bf
Matt Sacks <matt.s.sacks@gmail.com>
2016-08-29 14:08:43 -0700

    Remove test classname

--- addon/components/canvas-block/component.js
+++ addon/components/canvas-block/component.js
@@ -8,7 +8,6 @@ import Ember from 'ember';
  */
 export default Ember.Component.extend({
   classNames: ['canvas-block'],
-  localClassNames: 'component',
 
   /**
    * A dummy handler for an action that receives a block after its content has

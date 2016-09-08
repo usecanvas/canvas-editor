1ecf5286abeb01d6696fba102abebd7018a06565
Jonathan Clem <jonathan@jclem.net>
2016-09-08 16:05:04 -0400

    Add a dummy list

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -1,4 +1,7 @@
 import Paragraph from 'canvas-editor/lib/realtime-canvas/paragraph';
+import List from 'canvas-editor/lib/realtime-canvas/list';
+import ULItem from 'canvas-editor/lib/realtime-canvas/unordered-list-item';
+import CLItem from 'canvas-editor/lib/realtime-canvas/checklist-item';
 import Ember from 'ember';
 import RealtimeCanvas from 'canvas-editor/lib/realtime-canvas';
 import Title from 'canvas-editor/lib/realtime-canvas/title';
@@ -9,21 +12,31 @@ export default Ember.Route.extend({
   model() {
     return RealtimeCanvas.create({
       blocks: A([
-        Title.create({ content: A(['The Title']) }),
-        Paragraph.create({ content: A(['Hello, World']) }),
-        Paragraph.create({ content: A(['Hello, World']) }),
-        Paragraph.create({ content: A(['Hello, World']) }),
-        Paragraph.create({ content: A(['Hello, World']) }),
-        Paragraph.create({ content: A(['Hello, World']) }),
-        Paragraph.create({ content: A(['Hello, World']) }),
-        Paragraph.create({ content: A(['Hello, World']) }),
-        Paragraph.create({ content: A(['Hello, World']) }),
-        Paragraph.create({ content: A(['Hello, World']) }),
-        Paragraph.create({ content: A(['Hello, World']) }),
-        Paragraph.create({ content: A(['Hello, World']) }),
-        Paragraph.create({ content: A(['Hello, World']) }),
-        Paragraph.create({ content: A(['Foo Bar Baz']) }),
-        Paragraph.create({ content: A(["Kitsch +1 tousled cliche. Lumbersexual distillery keffiyeh flannel, green juice put a bird on it ugh williamsburg mixtape art party. Yuccie skateboard bicycle rights cardigan, kickstarter vinyl knausgaard mumblecore four loko wayfarers salvia before they sold out. Flannel gluten-free pabst umami. Hammock before they sold out deep v, selfies knausgaard normcore gluten-free chambray helvetica readymade trust fund cliche health goth. Literally twee poutine umami, forage next level crucifix gluten-free ennui. Banjo +1 quinoa blue bottle cray tilde, selvage church-key meditation lomo sriracha bitters truffaut tattooed."]) })
+        Title.create({ content: 'The Title' }),
+        Paragraph.create({ content: 'Hello, World' }),
+        Paragraph.create({ content: 'Hello, World' }),
+        Paragraph.create({ content: 'Hello, World' }),
+        Paragraph.create({ content: 'Hello, World' }),
+        Paragraph.create({ content: 'Hello, World' }),
+        List.create({
+          blocks: A([
+            ULItem.create({ content: 'List item A' }),
+            ULItem.create({ content: 'List item B' }),
+            ULItem.create({ content: 'List item C' }),
+            CLItem.create({ content: 'List item D', meta: { checked: true } }),
+            CLItem.create({ content: 'List item E', meta: { checked: false } }),
+            ULItem.create({ content: 'List item F' })
+          ])
+        }),
+        Paragraph.create({ content: 'Hello, World' }),
+        Paragraph.create({ content: 'Hello, World' }),
+        Paragraph.create({ content: 'Hello, World' }),
+        Paragraph.create({ content: 'Hello, World' }),
+        Paragraph.create({ content: 'Hello, World' }),
+        Paragraph.create({ content: 'Hello, World' }),
+        Paragraph.create({ content: 'Hello, World' }),
+        Paragraph.create({ content: 'Foo Bar Baz' }),
+        Paragraph.create({ content: "Kitsch +1 tousled cliche. Lumbersexual distillery keffiyeh flannel, green juice put a bird on it ugh williamsburg mixtape art party. Yuccie skateboard bicycle rights cardigan, kickstarter vinyl knausgaard mumblecore four loko wayfarers salvia before they sold out. Flannel gluten-free pabst umami. Hammock before they sold out deep v, selfies knausgaard normcore gluten-free chambray helvetica readymade trust fund cliche health goth. Literally twee poutine umami, forage next level crucifix gluten-free ennui. Banjo +1 quinoa blue bottle cray tilde, selvage church-key meditation lomo sriracha bitters truffaut tattooed." })
       ])
     });
   }

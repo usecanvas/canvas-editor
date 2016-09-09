ceadaf086ff584eaae25917d92492023811c1ec4
Jonathan Clem <jonathan@jclem.net>
2016-09-09 12:38:37 -0400

    Document `creatFromMarkdown`

--- addon/lib/realtime-canvas/block.js
+++ addon/lib/realtime-canvas/block.js
@@ -12,4 +12,31 @@ const { computed } = Ember;
 export default Ember.Object.extend({
   id: computed(_ => Base62UUID.generate()),
   meta: computed(_ => Ember.Object.create())
+}).reopenClass({
+  /* eslint-disable no-unused-vars */
+  /**
+   * Create a new instance of this class from the given Markdown string and with
+   * the given properties.
+   *
+   * This method parses the given markdown (and assumes it matches this block
+   * type) and creates a new instance of the class based off of it. It also
+   * accepts additional properties which can be applied.
+   *
+   * ```javascript
+   * ChecklistItem.createFromMarkdown(
+   *   '- [X] Foo',
+   *   { id: '123' });
+   *
+   * // Instance of a checked checklist item with ID '123'.
+   * ```
+   *
+   * @method
+   * @static
+   * @param {string} markdown The Markdown used to create the block
+   * @param {object} properties Additional properties to apply to the block
+   */
+  createFromMarkdown(markdown, properties) {
+    throw new Error('Class has not implemented `createFromMarkdown`.');
+  }
+  /* eslint-enable no-unused-vars */
 });

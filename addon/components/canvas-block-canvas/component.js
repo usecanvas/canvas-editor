0579ae7ca184ab9f0166bbe2f8a43d5e71e1b6d7
Jonathan Clem <jotclem@gmail.com>
2016-09-12 15:21:34 -0700

    Add progress to canvas-block-canvas

--- addon/components/canvas-block-canvas/component.js
+++ addon/components/canvas-block-canvas/component.js
@@ -1,6 +1,9 @@
 import CardBlock from 'canvas-editor/components/canvas-block-card/component';
+import Ember from 'ember';
 import layout from './template';
 import styles from './styles';
+
+const { computed } = Ember;
 
 /**
  * A component representing a Canvas card.
@@ -12,5 +15,13 @@ export default CardBlock.extend({
   classNames: ['canvas-block-canvas'],
   layout,
   localClassNames: ['component'],
-  styles
+  styles,
+
+  progress: computed('tasksComplete', 'tasksTotal', function() {
+    let tasksComplete = this.get('tasksComplete');
+    let tasksTotal = this.get('tasksTotal');
+    if (typeof tasksComplete !== 'number') tasksComplete = 1;
+    if (typeof tasksTotal !== 'number') tasksTotal = 1;
+    return tasksComplete / tasksTotal * 100;
+  })
 });

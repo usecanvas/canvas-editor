63b88685b54a1d4d0cfb10762e0177759458f17f
Max Schoening <max@max.wtf>
2016-09-19 15:58:13 -0700

    Add OKR template

--- addon/lib/templates/okr.js
+++ addon/lib/templates/okr.js
@@ -1,15 +1,59 @@
 export default {
   title: 'OKRs',
-  blocks: [{
-    type: 'paragraph',
-    content: 'Foo bar',
-    meta: {}
-  }, {
-    type: 'list',
-    blocks: [{
-      type: 'checklist-item',
-      content: 'CL Item',
-      meta: { checked: true }
-    }]
-  }]
+  blocks: [
+    {
+      type: 'heading',
+      content: '',
+      meta: {
+        level: 2,
+        placeholder: 'Objective #1'
+      }
+    },
+    {
+      type: 'tip',
+      content: 'Good OKRs are Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      meta: {}
+    },
+    {
+      type: 'paragraph',
+      content: '',
+      meta: {
+        placeholder: 'An introductory paragraph on the objective and why it matters to your team...'
+      }
+    },
+    {
+      type: 'heading',
+      content: 'Key Results',
+      meta: {
+        level: 3
+      }
+    },
+    {
+      type: 'list',
+      blocks: [
+        {
+          type: 'checklist-item',
+          content: '',
+          meta: {
+            placeholder: 'A key result...'
+          }
+        },
+        {
+          type: 'checklist-item',
+          content: '',
+          meta: {
+            placeholder: 'Another key result...'
+          }
+        }
+      ]
+    },
+    {
+      type: 'heading',
+      content: '',
+      meta: {
+        level: 2,
+        placeholder: 'Objective #2'
+      }
+    }
+  ]
 };

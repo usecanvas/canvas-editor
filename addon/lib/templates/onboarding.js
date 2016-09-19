ecb507474f7222ceb76822a4b86dc02e0bb06de3
Max Schoening <max@max.wtf>
2016-09-19 15:33:27 -0700

    Add onboarding checklist

--- addon/lib/templates/onboarding.js
+++ addon/lib/templates/onboarding.js
@@ -1,8 +1,96 @@
 export default {
   title: 'Onboarding',
-  blocks: [{
-    type: 'paragraph',
-    content: 'Paragraph content',
-    meta: {}
-  }]
+  blocks: [
+    {
+      type: 'heading',
+      content: 'HR Tools',
+      meta: {
+        level: 2
+      }
+    },
+    {
+      type: 'list',
+      blocks: [
+        {
+          type: 'checklist-item',
+          content: 'Zenpayroll',
+          meta: {}
+        },
+        {
+          type: 'checklist-item',
+          content: 'SimplyInsured',
+          meta: {}
+        },
+        {
+          type: 'checklist-item',
+          content: 'Stock',
+          meta: {}
+        }
+      ]
+    },
+    {
+      type: 'heading',
+      content: 'Accounts',
+      meta: {
+        level: 2
+      }
+    },
+    {
+      type: 'list',
+      blocks: [
+        {
+          type: 'checklist-item',
+          content: 'Slack',
+          meta: {}
+        },
+        {
+          type: 'checklist-item',
+          content: 'Google Apps',
+          meta: {}
+        },
+        {
+          type: 'checklist-item',
+          content: 'Abacus',
+          meta: {}
+        },
+        {
+          type: 'checklist-item',
+          content: 'Canvas org on Canvas',
+          meta: {}
+        }
+      ]
+    },
+    {
+      type: 'heading',
+      content: 'Developer Accounts',
+      meta: {
+        level: 2
+      }
+    },
+    {
+      type: 'list',
+      blocks: [
+        {
+          type: 'checklist-item',
+          content: 'Github org',
+          meta: {}
+        },
+        {
+          type: 'checklist-item',
+          content: 'Logentries',
+          meta: {}
+        },
+        {
+          type: 'checklist-item',
+          content: 'Abacus',
+          meta: {}
+        },
+        {
+          type: 'checklist-item',
+          content: 'Heroku org',
+          meta: {}
+        }
+      ]
+    },
+  ]
 };

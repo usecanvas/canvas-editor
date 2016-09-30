04d5a84760995350e58f123dff57e0f0411a1337
Max Schoening <max@max.wtf>
2016-09-30 11:10:20 -0700

    Name test correctly

--- tests/integration/components/canvas-editor/component-test.js
+++ tests/integration/components/canvas-editor/component-test.js
@@ -10,6 +10,6 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
   this.render(hbs`{{canvas-editor}}`);
-  percySnapshot('sample screenshot');
+  percySnapshot(assert);
   assert.equal(this.$().text().trim(), '');
 });

fd131357a214faedf6a7c5837cd9d94fcf458255
Jonathan Clem <jonathan@jclem.net>
2016-10-13 18:03:15 -0400

    Fix failing tests

--- tests/integration/components/canvas-block-code-content/component-test.js
+++ tests/integration/components/canvas-block-code-content/component-test.js
@@ -1,24 +1,16 @@
 import { moduleForComponent, test } from 'ember-qunit';
+import Ember from 'ember';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('canvas-block-code-content', 'Integration | Component | canvas block code content', {
+moduleForComponent('canvas-block-code-content',
+                   'Integration | Component | canvas block code content', {
   integration: true
 });
 
 test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
-
-  this.render(hbs`{{canvas-block-code-content}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#canvas-block-code-content}}
-      template block text
-    {{/canvas-block-code-content}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  this.set('block', Ember.Object.create({ content: 'foo' }));
+  this.render(hbs`{{canvas-block-code-content block=block}}`);
+  assert.equal(this.$().text().trim(), 'foo');
 });

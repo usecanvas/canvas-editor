de9d39695d0da3d9249519f54b2c8dbaa06c6ac6
Max Schoening <max@max.wtf>
2016-10-15 12:00:08 -0700

    Remove haneous iframe code

--- addon/components/canvas-block-url-gist/component.js
+++ addon/components/canvas-block-url-gist/component.js
@@ -6,30 +6,11 @@ const { computed, on } = Ember;
 
 export default Ember.Component.extend({
   classNames: ['canvas-block-url-gist'],
-  gist: computed.oneWay('unfurled.url'),
   layout,
   localClassNames: ['canvas-block-url-gist'],
   styles,
 
-  updateIframeContent: on('didInsertElement', function() {
-    const iframe = document.createElement('iframe');
-    const iframeId = `gist-${Ember.guidFor(this)}`;
-    iframe.id = iframeId;
-    this.$('.gist').append(iframe);
-
-    const script = `<script src="${this.get('gist')}.js"></script>`;
-    const resizeFrame = `parent.document.querySelector('#${iframeId}').style.height = document.body.scrollHeight + 'px';`; // eslint-disable-line max-len
-    const html = `<html><body onload="${resizeFrame}">${script}</body></html>`;
-
-    let iframeDoc = iframe.document;
-    if (iframe.contentDocument) {
-      iframeDoc = iframe.contentDocument;
-    } else if (iframe.contentWindow) {
-      iframeDoc = iframe.contentWindow.document;
-    }
-
-    iframeDoc.open();
-    iframeDoc.writeln(html);
-    iframeDoc.close();
-  })
+  html: computed.oneWay('unfurled.html', function() {
+    return Ember.String.htmlSafe('unfurled.html');
+  }
 });

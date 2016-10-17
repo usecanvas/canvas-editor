fd3607f47106e84e8800ab5add42d5f6b6e4fe07
Max Schoening <max@max.wtf>
2016-10-17 11:18:15 -0400

    Fix tests

--- addon/components/canvas-block-url-github-gist/component.js
+++ addon/components/canvas-block-url-github-gist/component.js
@@ -2,8 +2,6 @@ import Ember from 'ember';
 import layout from './template';
 import styles from 'canvas-editor/components/canvas-block-url-github-gist/styles';
 
-const { computed } = Ember;
-
 export default Ember.Component.extend({
   classNames: ['canvas-block-url-github-gist'],
   layout,

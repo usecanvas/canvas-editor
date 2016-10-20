f6d6a3f39e453d2f04e9a2ccb359d36999a8ef7c
Jonathan Clem <jotclem@gmail.com>
2016-10-20 19:15:12 -0400

    Get only Canvas ID for links

--- addon/components/canvas-block-url-canvas/component.js
+++ addon/components/canvas-block-url-canvas/component.js
@@ -6,6 +6,7 @@ const { computed } = Ember;
 
 const TASKS_COMPLETE = 'Tasks Complete';
 const TASKS_TOTAL = 'Tasks Total';
+const CANVAS_ID_REGEX = /^https?:\/\/[^/]+\/[^/]+\/([^/]{22})/;
 
 export default Ember.Component.extend({
   classNames: ['canvas-block-url-canvas'],
@@ -14,7 +15,7 @@ export default Ember.Component.extend({
   styles,
 
   canvasId: computed('unfurled.url', function() {
-    return this.get('unfurled.url').split('/')[4];
+    return this.get('unfurled.url').match(CANVAS_ID_REGEX)[1];
   }),
 
   hasProgress: computed('progress', function() {

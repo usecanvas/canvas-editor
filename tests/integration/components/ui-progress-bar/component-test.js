0e813dcb6717f7510098a1d605095b484c8a7fe3
Max Schoening <max@max.wtf>
2016-10-31 13:46:45 -0700

    Add percy test for ui-progress-bar

--- tests/integration/components/ui-progress-bar/component-test.js
+++ tests/integration/components/ui-progress-bar/component-test.js
@@ -1,24 +1,25 @@
 import { moduleForComponent, test } from 'ember-qunit';
+import { percySnapshot } from 'ember-percy';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('ui-progress-bar', 'Integration | Component | ui progress bar', {
+moduleForComponent('ui-progress-bar',
+                   'Integration | Component | ui progress bar', {
   integration: true
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  this.set('progress', 25);
+  this.render(hbs`{{ui-progress-bar progress=progress}}`);
+  percySnapshot('ui-progress-bar at 25');
 
-  this.render(hbs`{{ui-progress-bar}}`);
+  this.set('progress', 75);
+  percySnapshot('ui-progress-bar at 75');
+
+  this.set('progress', 0);
+  percySnapshot('ui-progress-bar at 0');
+
+  this.set('progress', 100);
+  percySnapshot('ui-progress-bar at 100');
 
   assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#ui-progress-bar}}
-      template block text
-    {{/ui-progress-bar}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
 });

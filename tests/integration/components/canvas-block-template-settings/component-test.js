1d55dca2b9cfd288c441bf4902fe9ebb057e83df
Max Schoening <max@max.wtf>
2016-11-16 12:52:07 -0800

    Fix tests

--- tests/integration/components/canvas-block-template-settings/component-test.js
+++ tests/integration/components/canvas-block-template-settings/component-test.js
@@ -7,19 +7,6 @@ moduleForComponent('canvas-block-template-settings',
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
   this.render(hbs`{{canvas-block-template-settings}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#canvas-block-template-settings}}
-      template block text
-    {{/canvas-block-template-settings}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim(), 'Use as placeholder');
 });

da583292a86a3644a5f7bac5ccb1ee7006338ba8
Jonathan Clem <jonathan@jclem.net>
2016-12-05 12:07:07 -0500

    Document/reorganize copy-paste module

--- addon/lib/copy-paste.js
+++ addon/lib/copy-paste.js
@@ -1,31 +1,25 @@
 import RealtimeCanvas from './realtime-canvas';
 
-function cleanID(line) {
-  if (line.blocks) {
-    line.blocks = line.blocks.map(cleanID);
-  }
-  Reflect.deleteProperty(line, 'id');
-  return line;
-}
-
-function extractPasteData({ originalEvent: { clipboardData } }) {
-  const { types } = clipboardData;
-  if (types.includes('application/x-canvas')) {
-    return clipboardData.getData('application/x-canvas');
-  }
-  return clipboardData.getData('text/plain');
-}
-
+/**
+ * Class for extracting pasted RealtimeCanvas blocks from a `paste` event.
+ *
+ * @class CopyPaste
+ */
 export default class CopyPaste {
   constructor(evt) {
-    this._evt = evt;
     this.pasteData = extractPasteData(evt);
   }
 
+  /**
+   * @member {?CanvasEditor.RealtimeCanvas.Block} Blocks extracted from a
+   *   `paste` event
+   */
   get pastedLines() {
     try {
       const { lines, isInline } = JSON.parse(this.pasteData);
+
       if (isInline) return lines[0];
+
       return lines
         .map(cleanID)
         .map(json => RealtimeCanvas.createBlockFromJSON(json));
@@ -33,4 +27,23 @@ export default class CopyPaste {
       return null;
     }
   }
+}
+
+function cleanID(line) {
+  if (line.blocks) {
+    line.blocks = line.blocks.map(cleanID);
+  }
+
+  Reflect.deleteProperty(line, 'id');
+  return line;
+}
+
+function extractPasteData({ originalEvent: { clipboardData } }) {
+  const { types } = clipboardData;
+
+  if (types.includes('application/x-canvas')) {
+    return clipboardData.getData('application/x-canvas');
+  }
+
+  return clipboardData.getData('text/plain');
 }

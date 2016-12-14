5ff3b7596d4de4ac15f8c14908c229322170409b
Max Schoening <max@max.wtf>
2016-12-13 16:24:30 -0800

    Parse timestamps

--- addon/components/slack-message/component.js
+++ addon/components/slack-message/component.js
@@ -2,8 +2,14 @@ import Ember from 'ember';
 import layout from './template';
 import styles from './styles';
 
+const { computed } = Ember;
+
 export default Ember.Component.extend({
   layout,
   localClassNames: ['slack-message'],
-  styles
+  styles,
+
+  unixTimestamp: computed('message.timestamp', function() {
+    return parseFloat(this.get('message.timestamp')) * 1000;
+  })
 });

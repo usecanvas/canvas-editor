fc4037285efe2cf1cc41b476871f090daff22ee1
Jonathan Clem <clem@corn.local>
2016-12-14 16:04:13 -0800

    Fix markdown-it helper

--- tests/unit/helpers/markdown-it-test.js
+++ tests/unit/helpers/markdown-it-test.js
@@ -1,12 +1,10 @@
-
-import { markdownIt } from 'dummy/helpers/markdown-it';
+import { markdownItHelper } from 'dummy/helpers/markdown-it';
 import { module, test } from 'qunit';
 
 module('Unit | Helper | markdown it');
 
 // Replace this with your real tests.
 test('it works', function(assert) {
-  let result = markdownIt([42]);
+  const result = markdownItHelper([42]);
   assert.ok(result);
 });
-

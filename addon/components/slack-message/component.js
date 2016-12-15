3661cf45b41870dfb74a3afdceb24220481e799f
Jonathan Clem <clem@corn.local>
2016-12-14 16:19:07 -0800

    Document slack message component

--- addon/components/slack-message/component.js
+++ addon/components/slack-message/component.js
@@ -4,11 +4,21 @@ import styles from './styles';
 
 const { computed } = Ember;
 
+/**
+ * A computed which displays a Slack message
+ *
+ * @class CanvasEditor.SlackMessageComponent
+ * @extends Ember.Component
+ */
 export default Ember.Component.extend({
   layout,
   localClassNames: ['slack-message'],
   styles,
 
+  /**
+   * @property {number} The Slack timestamp converted to a Unix millisecond
+   *   timestamp value.
+   */
   unixTimestamp: computed('message.timestamp', function() {
     return parseFloat(this.get('message.timestamp')) * 1000;
   })

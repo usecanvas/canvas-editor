81f1389a048bfbcf190f65c821f61ca430516f5f
Jonathan Clem <clem@corn.local>
2016-12-14 16:26:35 -0800

    Document markdown-it helper

--- addon/helpers/markdown-it.js
+++ addon/helpers/markdown-it.js
@@ -1,8 +1,21 @@
 import Ember from 'ember';
 import MarkdownIt from 'markdown-it';
 
+/**
+ * Helper for rendering a Markdown string as HTML.
+ *
+ * @module CanvasEditor.MarkdownItHelper
+ */
+
 const renderer = new MarkdownIt();
 
+/**
+ * Convert a Markdown string to a safe HTML string.
+ *
+ * @function
+ * @param {Array<string>} params The params passed to the helper
+ * @param {Object} hash The options hash passed to the helper
+ */
 export function markdownIt([text], hash = {}) {
   let markdownHTML;
 

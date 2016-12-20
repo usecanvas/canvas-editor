c800553b8aaf5f09af937479c3ad55408f081bf0
Olivia Briggs <ofbriggs@gmail.com>
2016-12-20 03:21:55 -0500

    Add fixes to parser state code

--- addon/lib/symbol/parser.js
+++ addon/lib/symbol/parser.js
@@ -4,8 +4,9 @@ import URL from '../realtime-canvas/url-card';
 
 const LineReducer = {
   placeholder(acc, text) {
-    const [key, val] = text.slice(1, -1).split(':');
-    return ['initial', acc.concat({ key, val: val.trim() })];
+    const [key, ...vals] = text.slice(1, -1).split(':');
+    return ['initial',
+      acc.concat({ key: key.trim(), val: vals.join(':').trim() })];
   },
 
   initial(acc, text) {
@@ -17,12 +18,30 @@ const LineReducer = {
 };
 
 const ParseReducer = {
+  code(acc, text) {
+    const lastBlock = acc[acc.length - 1];
+    const block = parseBlock(text);
+    if (block.type === 'code') {
+      return ['initial', acc];
+    }
+    lastBlock.blocks = (lastBlock.blocks || []).concat(block);
+    return ['code', acc];
+  },
+
   initial(acc, text) {
-    const lineParser = buildParser(/(<[^:]*[^: \t]+[^:]*:[^>]+>)/, LineReducer);
-    const parsed = lineParser(text);
-    return ['initial', acc.concat(text)];
+    const block = parseBlock(text);
+    if (block.type === 'code') {
+      return ['code', acc.concat(block)];
+    } else if (isEmptyBlock(block)) {
+      return ['initial', acc];
+    }
+    return ['initial', acc.concat(block)];
   }
 };
+
+function isEmptyBlock({ type, content }) {
+  return type === 'paragraph' && content === '';
+}
 
 function buildParser(regex, reducer) {
   return text => text.split(regex).reduce(([parseState, acc], next) =>
@@ -31,18 +50,27 @@ function buildParser(regex, reducer) {
 }
 
 function parseBlock(text) {
+  const block = RealtimeCanvas.createBlockFromMarkdown(text);
   const lineParser = buildParser(/(<[^:]*[^: \t]+[^:]*:[^>]+>)/, LineReducer);
   const [first, ...rest] = lineParser(text);
-  if (typeof first === 'string' &&
-      rest.length === 0) return RealtimeCanvas.createBlockFromMarkdown(text);
-  if (rest.length === 0) return parsePlaceholder(first);
+  if (!first || typeof first === 'string' &&
+      rest.length === 0) return RealtimeCanvas.createBlockFromMarkdown(text)
+    .toJSON();
+  if (rest.length === 0) return parsePlaceholderBlock(first);
+  if (block.get('type') === 'list') {
+    const { type, meta, content } = block.get('blocks.0').toJSON();
+    const isContinuable = isContinuableList(first, rest);
+    return { type, meta, isContinuable, blocks: lineParser(content) };
+  }
+  const { type, meta, content } = block.toJSON();
+  return { type, meta, blocks: lineParser(content) };
 }
 
 function isContinuableList(first, rest) {
-  return /[ \t]*\* /.test(first);
+  return (/[ \t]*\+ /).test(first) && rest.length > 0;
 }
 
-function parsePlaceholder({ key, val }) {
+function parsePlaceholderBlock({ key, val }) {
   if (Image.pattern.test(val)) {
     return { type: 'placeholder-image', key, placeholder: val };
   } else if (URL.pattern.test(val)) {

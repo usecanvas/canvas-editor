b75925c10aace35ac968aa59107796a7c2dc9b63
Olivia Briggs <ofbriggs@gmail.com>
2016-12-29 10:52:18 -0500

    Improve firefox behaviour for inline content editable

--- addon/components/canvas-inline-editable/component.js
+++ addon/components/canvas-inline-editable/component.js
@@ -9,13 +9,52 @@ export default Ember.Component.extend({
   tagName: 'span',
   layout,
   styles,
-  counter: 0,
+
   input() {
     // Force a style recalculation to get rid of placeholder text in safari
-    const el = this.$('span')[0];
+    const el = this.contentElem();
     el.style.cssText += ';-webkit-transform:rotateZ(0deg)';
     Ember.K(el.offsetHeight);
     el.style.cssText += ';-webkit-transform:none';
+  },
+
+  doubleClick(evt) {
+    // When double clicking inside an inline contenteditable, firefox overselects and
+    // deletion no longer works, this method correctly selects the entire content
+    if (this.contentElem().textContent !== '') {
+      evt.preventDefault();
+      evt.stopPropagation();
+      this.selectAllContent();
+    }
+  },
+
+  mouseDown(evt) {
+    if (this.contentElem().textContent === '') {
+      const el = this.contentElem();
+      const sel = window.getSelection();
+      const rng = new Range();
+      evt.preventDefault();
+      evt.stopPropagation();
+      el.focus();
+      rng.setStart(el, 0);
+      sel.removeAllRanges();
+      sel.addRange(rng);
+    }
+  },
+
+  selectAllContent() {
+    const sel = window.getSelection();
+    const rng = new Range();
+    const el = this.contentElem();
+    el.focus();
+    rng.setStart(el, 0);
+    rng.setEnd(el, el.childNodes.length);
+    sel.removeAllRanges();
+    sel.addRange(rng);
+  },
+
+  contentElem() {
+    return this.$('span')[1];
   },
 
   keyDown(evt) {

73383cac2edeecfc6d2550cec85e812ec1600650
Jonathan Clem <jonathan@jclem.net>
2017-01-05 16:31:49 -0500

    Clean up CanvasInlineEditableComponent

--- addon/components/canvas-inline-editable/component.js
+++ addon/components/canvas-inline-editable/component.js
@@ -1,11 +1,9 @@
 import Ember from 'ember';
 import Key from 'canvas-editor/lib/key';
-import SelectionState from 'canvas-editor/lib/selection-state';
 import layout from './template';
 import styles from './styles';
 
-const isFirefox = window.navigator.userAgent.includes('Firefox');
-const { computed } = Ember;
+const { computed, observer, on, run } = Ember;
 
 /**
  * This component is a cross-browser contenteditable span that works around bad
@@ -13,17 +11,80 @@ const { computed } = Ember;
  * firefox and chrome figure out the navigation boundariers and a span
  * containing the placeholder text as the browsers break even more with
  * pseudoelements.
+ *
+ * @class CanvasEditor.CanvasInlineEditableComponent
+ * @extends Ember.Component
  */
 export default Ember.Component.extend({
-  classNames: ['canvas-inline-editable'],
-  localClassNames: ['canvas-inline-editable'],
-  tagName: 'span',
+  /*
+   * SIMPLE PROPERTIES
+   * =================
+   */
+
+  /**
+   * @member {object} The component layout template
+   */
   layout,
+
+  /**
+   * @member {Array<string>} Non-localized class names
+   */
+  localClassNames: ['canvas-inline-editable'],
+
+  /**
+   * @member {string} HTML tag name for this component
+   */
+  tagName: 'span',
+
+  /**
+   * @member {object} The component styles
+   */
   styles,
 
-  selectionState: computed(function() {
-    return new SelectionState(this.contentElem());
+  /*
+   * COMPUTED PROPERTIES
+   * ===================
+   */
+
+  /**
+   * @member {Element} The component's content element
+   */
+  contentElement: computed(function() {
+    return this.$(`.${this.styles.content}`).get(0);
+  }).volatile(),
+
+  /**
+   * @member {string} The component's content element content
+   */
+  contentElementText: computed(function() {
+    const element = this.get('contentElement');
+    return element.innerText || element.textContent;
+  }).volatile(),
+
+  /*
+   * METHODS
+   * =======
+   */
+
+  /*
+   * Observers
+   * ---------
+   */
+
+  /**
+   * Observer called when the symbol value changes to (potentially) update
+   * the DOM with the new value.
+   *
+   * @method
+   */
+  onSymbolValueChange: observer('symbolValue', function() {
+    this.initContentElementText();
   }),
+
+  /*
+   * DOM Event Hooks
+   * ---------------
+   */
 
   /**
    * Handle an input event in the inline contenteditable.
@@ -34,61 +95,42 @@ export default Ember.Component.extend({
   input(evt) {
     evt.stopPropagation();
     // Force a style recalculation to get rid of placeholder text in safari
-    const el = this.contentElem();
+    const el = this.get('contentElement');
     el.style.cssText += ';-webkit-transform:rotateZ(0deg)';
     Ember.K(el.offsetHeight);
     el.style.cssText += ';-webkit-transform:none';
 
-    const elem = this.$().find(`.${this.styles.content}`).get(0);
-    const text = elem.innerText || elem.textContent;
+    const text = this.get('contentElementText');
 
+    this.set('isUpdatingContent', true);
     this.get('onInput')(this.get('chunk'), text);
+    run.scheduleOnce('afterRender', this, 'set', 'isUpdatingContent', false);
   },
 
+  /**
+   * Handle a doubleclick in the inline contenteditable.
+   *
+   * When double clicking inside an inline contenteditable, FireFox
+   * overselects and deletion no longer works, this method correctly selects the
+   * entire content
+   *
+   * @method
+   * @param {jQuery.Event} evt The dbclick event
+   */
   doubleClick(evt) {
-    // When double clicking inside an inline contenteditable, firefox
-    // overselects and deletion no longer works, this method correctly selects
-    // the entire content
-    if (this.contentElem().textContent !== '') {
+    if (this.get('contentElementText') !== '') {
       evt.preventDefault();
       evt.stopPropagation();
       this.selectAllContent();
     }
   },
 
-  mouseDown(evt) {
-    if (this.contentElem().textContent === '') {
-      evt.preventDefault();
-      evt.stopPropagation();
-      this.selectAllContent();
-    }
-  },
-
-  selectAllContent() {
-    const sel = window.getSelection();
-    const rng = new Range();
-    const el = this.contentElem();
-    el.focus();
-    rng.setStart(el, 0);
-    rng.setEnd(el, el.childNodes.length);
-    sel.removeAllRanges();
-    sel.addRange(rng);
-  },
-
-  contentElem() {
-    return this.$('span')[1];
-  },
-
-  getElementText() {
-    const element = this.contentElem();
-    if (element.childNodes.length === 1 &&
-        element.firstChild.nodeName === 'BR') return '';
-    let text = element.innerText || element.textContent;
-    // Firefox appends a <br> to the end of contenteditable
-    if (isFirefox) text = text.replace(/\n$/, '');
-    return text;
-  },
-
+  /**
+   * Handle a keydown event and prevent "return".
+   *
+   * @method
+   * @param {jQuery.Event} evt The keydown event
+   */
   keyDown(evt) {
     const key = new Key(evt.originalEvent);
 
@@ -96,5 +138,67 @@ export default Ember.Component.extend({
       evt.stopPropagation();
       evt.preventDefault();
     }
+  },
+
+  /**
+   * Handle a mousedown event.
+   *
+   * @method
+   * @param {jQuery.Event} evt The mousedown event
+   */
+  mouseDown(evt) {
+    if (this.get('contentElementText') === '') {
+      evt.preventDefault();
+      evt.stopPropagation();
+      this.selectAllContent();
+    }
+  },
+
+  /*
+   * Lifecycle Hooks
+   * ---------------
+   */
+
+  /**
+   * Initialize the content element text by updating the DOM.
+   *
+   * @method
+   */
+  initContentElementText: on('didInsertElement', function() {
+    this.setContentElementText();
+  }),
+
+  /*
+   * Regular Methods
+   * ---------------
+   */
+
+  /**
+   * Select all content in the element.
+   *
+   * @method
+   */
+  selectAllContent() {
+    const sel = window.getSelection();
+    const rng = new Range();
+    const el = this.get('contentElement');
+    el.focus();
+    rng.setStart(el, 0);
+    rng.setEnd(el, el.childNodes.length);
+    sel.removeAllRanges();
+    sel.addRange(rng);
+  },
+
+  /**
+   * Set the content element text from the current symbol value if not
+   * mid-update.
+   *
+   * @method
+   */
+  setContentElementText() {
+    if (this.get('isUpdatingContent')) return;
+    const element = this.get('contentElement');
+    const value = this.get('symbolValue');
+    Ember.$(element).text(value);
   }
 });

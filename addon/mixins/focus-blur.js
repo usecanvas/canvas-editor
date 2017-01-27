132279cf46b049652884f9356835c6fe64f7286d
Olivia Briggs <ofbriggs@gmail.com>
2017-01-27 01:26:15 -0500

    Fix breaking .set call in Chrome Canary

--- addon/mixins/focus-blur.js
+++ addon/mixins/focus-blur.js
@@ -6,7 +6,12 @@ export default Ember.Mixin.create({
 
   actions: {
     onBlur() {
-      this.set('isFocused', false);
+      // If the blur event fires in the same the run loop as the
+      // contenteditable=false gets set then this will trigger a
+      // rerender in the render queue which is not allowed anymore.
+      Ember.run.schedule('afterRender', _ => {
+        this.set('isFocused', false);
+      });
     },
 
     onFocus() {
